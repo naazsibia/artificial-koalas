9de20d0f53cd079c54fbf32dfaad0fe51adafd45
Naaz Sibia <naazsibia@utmuser5-211.wireless.utoronto.ca>
2019-02-13 12:26:26 -0500

    Can Store Position
    Added a Position class to our Java project. This will help store positions of various Chess pieces and buttons.

---
+++ ChessGame/src/Game/Position.java
@@ -0,0 +1,34 @@
+package Game;
+/**
+ * This class stores the 2D Positon of an object. 
+ * @author naazsibia
+ *
+ */
+public class Position {
+	private int x;
+	private int y;
+	
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return this.x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	
+	
+}
