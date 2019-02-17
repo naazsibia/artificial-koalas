a6d073754e154700e9b555539030eac5144d072a
Author Naaz Sibia <sibianjali@gmail.com>
2019-02-17 17:49:04 -0500

    Implement first version of King class.
    Includes:
    - Private variable safe
    - Private variable checkmated
    - Overrides Piece Constructor
    - Public getter isSafe for safe
    - Public getter hasBeenCheckmated for checkmated
    - Public setter changeSafe to change boolean safe
    - Public setter changeCheckmated to change the boolean checkmated
    - Documentation for class and methods

---
+++ ChessGame/src/Game/King.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+
+/**
+ * This class allows making, selecting, moving, and killing a King. It will also help warn users in
+ * the game if a king is about to be checkmated. 
+ * @author naazsibia
+ *
+ */
+public class King extends Piece{
+	private boolean safe; // will keep track of whether king is safe or about to be checkmated
+	private boolean checkmated; // will keep track of whether king has been checkmated
+	/**
+	 * Makes a new King with the given Position and color.
+	 */
+	public King(Position position, int color) {
+		super(position, color);
+		safe = true;
+		checkmated = false;
+	}
+	
+	/**
+	 * Returns true iff King is safe.
+	 * @return safe: a boolean that represents whether the king is about to be checkmated
+	 */
+	public boolean isSafe() {
+		return safe;
+	}
+	/**
+	 * Returns true iff King has been checkmated.
+	 * @return checkmate: a boolean that represents whether the king has been checkmated
+	 */
+	public boolean isCheckmated() {
+		return checkmate;
+	}
+	
+	/**
+	 * Changes safety of King. If King was safe earlier, then safety is changed to false else 
+	 * it is changed to true.
+	 */
+	public void changeSafety() {
+		if(safe){
+			safe = false;
+		}
+		else{
+			safe = true;
+		}
+	}
+	
+	/**
+	 * Changes whether King has been checkmated. If the king was not checkmated earlier, this makes 
+	 * the king checkmated.
+	 */
+	public void changeCheckmated() {
+		if(!checkmated) {
+			checkmated = true;
+		}
+	}	
+}
