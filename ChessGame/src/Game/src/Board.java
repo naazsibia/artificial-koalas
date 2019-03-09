2b341a191333e59d801d340803c0a41505b04d58
jmacmartin <47605715+jmacmartin@users.noreply.github.com>
2019-03-09 13:46:16 -0500

    Update boardModel
    Update the Board class with the following chages:
    - boardModel is now a 8 by 8 double array
    - boardModel now uses PieceButton
    - On initalizatrion boardModel starts with a fully populated board
    - Add attribute "selectedPiece" which tracks piece is selected by User

--- ChessGame/src/Game/src/Board.java
+++ ChessGame/src/Game/src/Board.java
@@ -1,4 +1,4 @@
-package Game.src;
+package src;
 /**
  * This class functions as the game board and stores
  * information on the current board state
@@ -7,13 +7,23 @@ package Game.src;
  *   
  */
 public class Board {
-	private PieceHandler[] boardModel;
+	private PieceButton[][] boardModel;
 	private int currentPlayer;
+	private PieceButton selectedPiece;
 	
 	public Board() {
-		this.boardModel = new PieceHandler[32];
-		//TODO: Add each piece to the board
+		this.boardModel = new PieceButton[8][8];
+		// Create 64 PieceButtons, one for each position on the board 
+		for (int x = 0; x < 8; x = x+1) {
+			PieceButton[] column = new PieceButton[8];
+			for (int y = 0; y < 8; y = y+1) {
+				column[y] = new PieceButton(new Position(x, y), false);
+			}
+			this.boardModel[x] = column;
+		}
+		
 		this.currentPlayer = 1;
+		this.selectedPiece = null;
 	}
 	
 	public void switchTurn() {
@@ -24,11 +34,19 @@ public class Board {
 		}
 	}
 	
-	public PieceHandler[] getBoardModel() {
+	public PieceButton[][] getBoardModel() {
 		return this.boardModel;
 	}
 	
 	public int getCurrentPlayer() {
 		return this.currentPlayer;
+	}
+	
+	public PieceButton getSelectedPiece() {
+		return this.selectedPiece;
+	}
+	
+	public void setSelectedPiece(PieceButton pb) {
+		this.selectedPiece = pb;
 	}
 }
