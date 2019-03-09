1ebb9030e21902576c48d23ff449f17cf2ac8277
jmacmartin <47605715+jmacmartin@users.noreply.github.com>
2019-03-09 15:23:36 -0500

    Update Bishop
    Remove selectPiece() method
    Add toString() method

--- ChessGame/src/Game/Bishop.java
+++ ChessGame/src/Game/Bishop.java
@@ -1,10 +1,9 @@
+package Game;
 /**
  * This class allows making, selecting, moving, and killing a Bishop. 
  * @author zuqing li
  *
  */
-
-package Game;
 
 public class Bishop extends Piece{
 	/**
@@ -17,32 +16,8 @@ public class Bishop extends Piece{
 		
 	}
 	
-	
 	@Override
-	public void selectPiece() {
-		this.setSelected(true);
-		int currentX = this.getPosition().getX();
-		int currentY = this.getPosition().getY();
-		int index = 0;
-		Position[] validMoves = new Position[] {};
-		while(currentX != 0 && currentY != 0) {
-			validMoves[index] = new Position(currentX - 1, currentY - 1);
-			index++;
-		}
-		while(currentX != 7 && currentY != 7) {
-			validMoves[index] = new Position(currentX + 1, currentY + 1);
-			index++;
-		}
-		while(currentX != 0 && currentY != 7) {
-			validMoves[index] = new Position(currentX - 1, currentY + 1);
-			index++;
-		}
-		while(currentX != 7 && currentY != 0) {
-			validMoves[index] = new Position(currentX + 1, currentY - 1);
-			index++;
-		}
-		
+	public String toString() {
+		return "Bishop";
 	}
-
-	
 }
